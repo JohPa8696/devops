82e31ac445f9078c0ef86f4f5303002b7a3b7420
john pham <john.vanpham696@gmail.com>
2025-01-17 15:52:15 +1300

    added ways to authenticate to azure

--- provider.tf
+++ provider.tf
@@ -13,6 +13,11 @@ terraform {
 # Provider block specifies the AzureRM provider and its specific configurations
 provider "azurerm" {
   features {}
+  # Different ways to authenticate
+  # 1. Using az cli to login => for local development
+  # 2. Using managed identities: use_msi = true. MSI (example when you have a Azure devops agent running in a VM, you can use maanaged idenities to allow
+  #    the agent to authenticate to Azure subcrption. This way you don't need to store credentials in the terraform code or environment variable.)
+  # 3. Use service principal
   # Using environment variables to authenticate
 }
 
