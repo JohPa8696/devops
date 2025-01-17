c846e7699f79c81f426dceea408318cef81aa3ae
john pham <john.vanpham696@gmail.com>
2025-01-17 14:15:50 +1300

    added azure provider

---
+++ provider.tf
@@ -0,0 +1,18 @@
+# Terraform block specifies the required providers and their versions
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"  # The source is Hashicorp, meaning the provider is maintained by Hashicorp
+      version = "~> 3.0.2"  # It's better to lock the version to a specific version, because of potential breaking changes
+    }                       # ~> only allow the rightmost to increment.
+  }
+
+  required_version = ">=1.0.0"
+}
+
+# Provider block specifies the AzureRM provider and its specific configurations
+provider "azurerm" {
+  features {}
+}
+
+
