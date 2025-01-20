9553f2ef1ff9fb84194ad1d82822d4ab072d9352
john pham <john.vanpham696@gmail.com>
2025-01-20 22:51:54 +1300

    using for loop

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,15 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+// Using wild card to output storage names
+//output "storage_account_name" {
+//  # How to list multiple resources with count and output? 
+//  value = azurerm_storage_account.storages[*].name
+//}
+
+// Using for loop, mainly used for output.
 output "storage_account_name" {
-  # How to list multiple resources with count and output? 
-  value = azurerm_storage_account.storage[0].name
+    value = [for i in azurerm_storage_account.storages: i.name]
 }
 
 output "storage_account_endpoint" {
